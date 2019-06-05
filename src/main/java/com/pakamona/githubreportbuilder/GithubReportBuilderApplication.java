1d412a5be654b728c32e648bc610f47e46210484
pakamona <5346200@gmail.com>
2019-06-05 17:42:11 +0300

    Initial commit

---
+++ src/main/java/com/pakamona/githubreportbuilder/GithubReportBuilderApplication.java
@@ -0,0 +1,13 @@
+package com.pakamona.githubreportbuilder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubReportBuilderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubReportBuilderApplication.class, args);
+	}
+
+}
